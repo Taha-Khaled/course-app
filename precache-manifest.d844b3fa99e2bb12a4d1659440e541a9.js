898d3ec5222ae240d13c021401faedee359656ad
taha apps <appsbytaha@gmail.com>
2021-07-19 16:28:59 +0200

    Updates

---
+++ precache-manifest.d844b3fa99e2bb12a4d1659440e541a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbdf4247f62312cb8dc50a15c5d6c911",
+    "url": "/course-app/index.html"
+  },
+  {
+    "revision": "d52be280ab37a94328cc",
+    "url": "/course-app/static/css/main.d5e75890.chunk.css"
+  },
+  {
+    "revision": "f42cec02a93360bc0351",
+    "url": "/course-app/static/js/2.0f5cdc35.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/course-app/static/js/2.0f5cdc35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d52be280ab37a94328cc",
+    "url": "/course-app/static/js/main.b2f45aac.chunk.js"
+  },
+  {
+    "revision": "061237ebbb60eec1526c",
+    "url": "/course-app/static/js/runtime-main.dfda4914.js"
+  }
+]);
